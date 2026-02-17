e2ce04ab736e7ad42b4504d134fb3f189d0448ef
ardeleanvalentin <ardeleanvalentin490@yahoo.com>
2026-02-17 19:38:04 +0200

    Reparare spatii fisiere

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-
 def adunare(a, b):
     return a + b
 
 
 def scadere(a, b):
     return a - b
-
-
--- test_app.py
+++ test_app.py
@@ -1,10 +1,9 @@
-
 from app import adunare, scadere
 
 
 def test_adunare():
     assert adunare(2, 3) == 5
-    
+
 
 def test_scadere():
     assert scadere(5, 2) == 3
