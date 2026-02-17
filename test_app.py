8a0e8dc6d4f791514f872eadeb016c274a55fc61
Valentin <ardeleanvalentin490@yahoo.com>
2026-02-17 20:07:45 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,4 +6,5 @@ def test_adunare():
 
 
 def test_scadere():
-    assert scadere(5, 2) == 3  # Comentariu corect cu doua spatii inainte sdfgersgwergergg
+    # Comentariul este acum deasupra pentru a evita linia prea lunga
+    assert scadere(5, 2) == 3
